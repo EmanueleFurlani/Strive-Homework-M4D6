af2f62d901910e5627f93448fad6859d2e403686
emanuele <emanuelefurlani.go@gmail.com>
2021-08-30 16:49:53 +0200

    part of ex 1 and ex 2 of strivebook app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import WarningSign from './components/WarningSign'
+import MyBadge from './components/MyBadge'
+import SingleBook from './components/SingleBook'
+import BookList from './components/BookList'
+import fantasyBooks from './fantasyBooks.json'
+import MyNav from './components/MyNav'
+import MyJumbotron from './components/MyJumbotron'
+import MyFooter from './components/MyFooter'
 
 function App() {
   return (
     <div className="App">
+        <MyNav title="Stribook" color="dark"/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*<MyJumbotron />*/}
+        {/* <WarningSign text="Watch out again!" /> */}
+        {/* <MyBadge text="NEW!!" color="info" /> */}
+        {/* <SingleBook book={fantasyBooks[0]} /> */}
+        <div /*id="over"*/>
+          <BookList books={fantasyBooks} />
+        </div>
       </header>
+       <MyFooter />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/AddComment.jsx
@@ -0,0 +1,79 @@
+import { Component } from "react";
+import { Button, Form } from 'react-bootstrap'
+
+class AddComment extends Component {
+
+    state = {
+        comment: {
+            comment: '',
+            rate: 1,
+            elementId: this.props.asin
+        }
+    }
+
+    funcionComment = async (e) => {
+        e.preventDefault()
+        try {
+            let response = await fetch('https://striveschool-api.herokuapp.com/api/comments', {
+                method: 'POST',
+                body: JSON.stringify(this.state.comment),
+                headers: {
+                    'Content-type': 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZmM3YzJkNTI2MjAwMTViNmRjYWMiLCJpYXQiOjE2MjkyODk1OTYsImV4cCI6MTYzMDQ5OTE5Nn0.iDlMUTTc0xtrNaoAmVgV2jnkXWgsEQjde8S63n28N4U'
+                }
+            })
+            if (response.ok) {
+                alert('Comment was ok!')
+            } else {
+                console.log('error')
+                alert('Oh nooooo!')
+            }
+        } catch (error) {
+            console.log('error')
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.functionComment}>
+                    <Form.Group>
+                        <Form.Label>Comment here</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            value={this.state.comment.comment}
+                            onChange={e => this.setState({
+                                comment: {
+                                    ...this.state.comment,
+                                    comment: e.target.value
+                                }
+                            })}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Rating</Form.Label>
+                        <Form.Control as="select" value={this.state.comment.rate}
+                            onChange={e => this.setState({
+                                comment: {
+                                    ...this.state.comment,
+                                    rate: e.target.value
+                                }
+                            })}>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Button variant="success" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        )
+    }
+}
+
+export default AddComment
